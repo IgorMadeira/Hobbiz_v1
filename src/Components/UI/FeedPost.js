f0157ab6755c198373e7694f71082bbed8638fa3
IgorMadeira <igoramoliveira@hotmail.com>
2021-09-01 10:54:18 -0300

    Feed post design improvements

--- src/Components/UI/FeedPost.js
+++ src/Components/UI/FeedPost.js
@@ -6,37 +6,19 @@ import { BsFillStarFill } from "react-icons/bs";
 const FeedPost = (props) => {
   return (
     <>
-      <div className={styles["feed-post-profile-picture"]}>
-        <img
-          className={styles["feed-profile-picture-image"]}
-          src={profilePicture}
-          alt="Profile picture"
-          width={100}
-          height={100}
-        />
-      </div>
       <div className={styles["feed-post"]}>
-        <div className="feed-post-username">Username Placeholder</div>
-        <div className={styles["feed-post-user-stars"]}>
-          <BsFillStarFill />
-          <BsFillStarFill />
-          <BsFillStarFill />
-          <BsFillStarFill />
-          <BsFillStarFill />
-        </div>
-      </div>
+        <div className={styles["feed-post-username"]}>Username Placeholder</div>
 
-      <div className={styles["feed-post-profile-picture"]}>
-        <img
-          className={styles["feed-profile-picture-image"]}
-          src={profilePictureII}
-          alt="Profile picture"
-          width={100}
-          height={100}
-        />
-      </div>
-      <div className={styles["feed-post"]}>
-        <div className="feed-post-username">Username Placeholder</div>
+        <div className={styles["feed-post-profile-picture"]}>
+          <img
+            className={styles["feed-profile-picture-image"]}
+            src={profilePicture}
+            alt="Profile picture"
+            width={100}
+            height={100}
+          />
+        </div>
+
         <div className={styles["feed-post-user-stars"]}>
           <BsFillStarFill />
           <BsFillStarFill />
